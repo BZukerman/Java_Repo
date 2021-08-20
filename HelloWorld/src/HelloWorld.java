4d55561dcb4775b10b0c3126d582dfdd68cd9085
BZukerman <BZukerman@mail.ru>
2021-08-20 10:55:09 +0500

    20/08/2021

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,15 @@
+/**
+ * A program that prints row "Hello World!"
+ * @author Tsukerman Boris
+ * @version 1.0
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        // Single line comment
+
+        /*
+        Multi-line comment
+         */
+    }
+}
