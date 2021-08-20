c53e71c569f7cb85cfbbc7b11a529c434bc56425
BZukerman <BZukerman@mail.ru>
2021-08-20 11:07:09 +0500

    20/08/2021

---
+++ Itsalive/src/Itsalive.java
@@ -0,0 +1,8 @@
+public class Itsalive {
+    public static void main (String[] args) {
+        for (int count = 1; count <= 2; count++){
+            System.out.print("It's alive! ");
+        }
+        System.exit(77);
+    }
+}
