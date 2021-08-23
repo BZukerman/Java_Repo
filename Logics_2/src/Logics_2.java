ec90b1b225004e76cffd90ef5308fd3dd045e8fc
BZukerman <BZukerman@mail.ru>
2021-08-23 20:44:33 +0500

    23.08.2021

---
+++ Logics_2/src/Logics_2.java
@@ -0,0 +1,18 @@
+/*
+  Реализуйте метод, возвращающий true, если среди четырех его аргументов ровно
+  два истинны (любые).
+  Во всех остальных случаях метод должен возвращать false.
+ */
+
+public class Logics_2 {
+    boolean a;
+    boolean b;
+    boolean c;
+    boolean d;
+    boolean result;
+    a = ;
+    b = ;
+    c = ;
+    d = ;
+    result = ;
+}
