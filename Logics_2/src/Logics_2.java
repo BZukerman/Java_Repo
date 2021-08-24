b521e0f05f54e51145d2d023f47d626ff656ccb8
BZukerman <BZukerman@mail.ru>
2021-08-24 21:12:55 +0500

    24.08.2021 Как ввести true/false ?

--- Logics_2/src/Logics_2.java
+++ Logics_2/src/Logics_2.java
@@ -5,14 +5,42 @@
  */
 
 public class Logics_2 {
-    boolean a;
-    boolean b;
-    boolean c;
-    boolean d;
-    boolean result;
-    a = ;
-    b = ;
-    c = ;
-    d = ;
-    result = ;
-}
+    public static void main(String[] args) {
+        boolean a;
+        boolean b;
+        boolean c;
+        boolean d;
+        boolean result;
+        a = false;       /* int x = System.in.read(); */
+        b = false;       /* System.in.nextBoolean(); */
+        c = false;        /* System.in.nextBoolean(); */
+        d = true;        /* System.in.nextBoolean(); */
+        boolean ar[];    /* {false, false, false, false}; */
+        ar = new boolean[5];
+        ar[0] = a;
+        ar[1] = b;
+        ar[2] = c;
+        ar[3] = d;
+        int hel[];
+        hel = new int[5];
+        int summa = 0;
+        for (int i = 0; i < 4; i++) {
+            if (ar[i] == true) {
+            hel[i] = 1;
+            }
+        else {
+            hel[i] = 0;
+            };
+        summa = summa + hel[i];
+    }
+    /* summa = sum(help); */
+        if (summa != 2) {
+            result = false;
+        }
+        else {
+            result = true;
+        };
+/*    return result; */
+        System.out.print(result);
+        }
+    }
