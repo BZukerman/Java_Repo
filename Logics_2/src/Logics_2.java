3d6b9409b9438b3977df8a4cea473f4fddd32210
BZukerman <BZukerman@mail.ru>
2021-08-25 20:57:14 +0500

    24.08.2021 25.08.2021

--- Logics_2/src/Logics_2.java
+++ Logics_2/src/Logics_2.java
@@ -1,43 +1,83 @@
 /*
-  Реализуйте метод, возвращающий true, если среди четырех его аргументов ровно
-  два истинны (любые).
+  Реализуйте метод, возвращающий true, если среди четырех его аргументов
+  ровно два истинны (любые).
   Во всех остальных случаях метод должен возвращать false.
  */
 
+import java.util.Scanner;
+
 public class Logics_2 {
     public static void main(String[] args) {
-        boolean a;
-        boolean b;
-        boolean c;
-        boolean d;
-        boolean result;
-        a = false;       /* int x = System.in.read(); */
-        b = false;       /* System.in.nextBoolean(); */
-        c = false;        /* System.in.nextBoolean(); */
-        d = true;        /* System.in.nextBoolean(); */
-        boolean ar[];    /* {false, false, false, false}; */
-        ar = new boolean[5];
-        ar[0] = a;
-        ar[1] = b;
-        ar[2] = c;
-        ar[3] = d;
-        int hel[];
-        hel = new int[5];
+        Scanner sc = new Scanner(System.in);
+        String ah, bh, ch, dh;
+        boolean a = false, b = false, c = false, d = false, result = false;
+        boolean ar [];
+        ar = new boolean [4];
         int summa = 0;
-        for (int i = 0; i < 4; i++) {
-            if (ar[i] == true) {
-            hel[i] = 1;
-            }
-        else {
-            hel[i] = 0;
+
+        System.out.println("Введите cимвол t для true или f для false:");
+
+        ah = sc.nextLine();
+        bh = sc.nextLine();
+        ch = sc.nextLine();
+        dh = sc.nextLine();
+
+        if (ah.matches ("t")) {
+            a = true;
             };
-        summa = summa + hel[i];
-    }
+        if (ah.matches ("f")) {
+            a = false;
+            };
+/*        System.out.println(a);  */
+        if (bh.matches ("t")) {
+            b = true;
+            };
+        if (bh.matches ("f")) {
+            b = false;
+            };
+/*        System.out.println(b);  */
+        if (ch.matches ("t")) {
+            c = true;
+            };
+        if (ch.matches ("f")) {
+            c = false;
+            };
+/*        System.out.println(c);  */
+        if (dh.matches ("t")) {
+            d = true;
+            };
+        if (dh.matches ("f")) {
+            d = false;
+            };
+/*        System.out.println(d);  */
+
+        ar[0] = a;
+/*        System.out.println(ar[0]);  */
+        ar[1] = b;
+/*        System.out.println(ar[1]);  */
+        ar[2] = c;
+/*        System.out.println(ar[2]);  */
+        ar[3] = d;
+/*        System.out.println(ar[3]);  */
+        int hel[];
+        hel = new int[4];
+        for (int i = 0; i <= 3; i++) {
+            if (ar[i] == true) {
+                hel[i] = 1;
+                };
+            if (ar[i] == false) {
+                hel[i] = 0;
+                };
+/*            System.out.println(hel[i]);  */
+            summa = summa + hel[i];
+/*            System.out.println(summa);  */
+        }
+/*        System.out.println(summa);  */
     /* summa = sum(help); */
         if (summa != 2) {
             result = false;
-        }
-        else {
+            }
+        if (summa == 2) {
             result = true;
         };
 /*    return result; */
