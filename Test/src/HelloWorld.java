d5ce808b022ec6ec0531c0a5998a10f76149c70f
BZukerman <BZukerman@mail.ru>
2021-08-26 11:16:46 +0500

    20/08/2021 26.08.2021. Добавлены комментарии

--- Test/src/HelloWorld.java
+++ Test/src/HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        int Stat = 77;
-        System.out.println("Hello world!");
-        System.exit(Stat);
+        int Stat = 77;          // Код завершения
+        System.out.println("Hello world!");     // Печать результата
+        System.exit(Stat);          // Передача кода завершения
     }
 }
