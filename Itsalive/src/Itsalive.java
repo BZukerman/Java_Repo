7d2629669a82b6c1198a774d783d18eefa697524
BZukerman <BZukerman@mail.ru>
2021-08-26 11:09:41 +0500

    20/08/2021 26.08.2021. Вставлены комменты

--- Itsalive/src/Itsalive.java
+++ Itsalive/src/Itsalive.java
@@ -1,8 +1,8 @@
 public class Itsalive {
     public static void main (String[] args) {
         for (int count = 1; count <= 2; count++){
-            System.out.print("It's alive! ");
+            System.out.print("It's alive! ");   // Печать в цикле в 2 строку
         }
-        System.exit(77);
+        System.exit(77);                    // Код возврата
     }
 }
