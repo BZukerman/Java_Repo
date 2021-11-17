4620d8c297666b772914578474592f6a40b1fcc9
BZukerman <BZukerman@mail.ru>
2021-11-17 12:41:28 +0500

    17.11.2021. Финальный вариант прямого вычисления факториала

--- Factorial_2/src/Tsuker/Main.java
+++ Factorial_2/src/Tsuker/Main.java
@@ -1,6 +1,7 @@
 /* Реализуйте метод, вычисляющий факториал заданного натурального числа.
    Поскольку это очень быстро растущая функция будем использовать BigInteger.
    Вычисляется факториал в цикле от 2 до number (был цикл от 1)
+   Добавлен оператор switch (0 ==> 1; 1 ==> 1; value > 1 ==> вычисление N!)
 */
 package Tsuker;
 
@@ -21,14 +22,23 @@ public class Main {
 
     public static BigInteger factorial(int value) {
         int i;                                      // Целочисленный счетчик цикла
-        BigInteger mult1, mult2, result;            // Переменные типа BigInteger
+        BigInteger mult1, mult2, result;            // Переменныe типа BigInteger
+
+        switch (value) {                            // Ветвление по value
+            case 0:                                 // Для введенного значения 0
+                return new BigInteger("1");     // Возврат 1
+            case 1:                                 // Для введенного значения 1
+                return new BigInteger("1");     // Возврат 1
+            default:                                // По умолчанию для введенного value > 1
+        }                                           // выполняются действия ниже:
+
         mult1 = new BigInteger("1");            // Инициализация первого сомножителя
         result = mult1;                             // Инициализация результата перед циклом
         for (i = 2; i <= value; i++) {              // Цикл можно начинать с i = 2; а не с i = 1;
             mult2 = BigInteger.valueOf(i);          // Второй сомножитель (преобразование int в BigInteger)
             result = mult1.multiply(mult2);         // Результат произведения (умножение * здесь не работает!)
             mult1 = result;                         // Первый сомножитель для следующей итерации
-            }
+        }
 
         return result;                              // Возврат результата произведения
     }
